15972fa98d48666f7b927120e4e396bde5a194b7
JorgeCastillo700 <38119608+JorgeCastillo700@users.noreply.github.com>
2022-02-02 18:02:40 -0800

    Update appcenter-pre-build.sh

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
-#
-# Download the latest state (`develop` branch) of the iOS SDK https://github.com/Microsoft/AppCenter-SDK-Apple
-# And overwrite the files in the respective location of the Xcode project.
 
-if [ "$APPCENTER_BRANCH" == "develop" ];
-then
-    cd $APPCENTER_SOURCE_DIRECTORY/Pods/AppCenter/
-    curl -O https://mobilecentersdkdev.blob.core.windows.net/sdk/AppCenter-SDK-Apple.zip
-    unzip -o AppCenter-SDK-Apple.zip
-fi
+# Updating Info.plist
 
+echo "Updating Info.plist"
+
+PLIST=$BUILD_REPOSITORY_LOCALPATH/Restart/Info.plist
 VERSION_CODE=$((VERSION_CODE_SHIFT + APPCENTER_BUILD_ID))
-plutil -replace CFBundleVersion -string "$VERSION_CODE"
-$APPCENTER_SOURCE_DIRECTORY/Restart/Info.plist
+
+echo "VERSION CODE : $.{VERSION_CODE}"
+
+/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString 1.0.${VERSION_CODE}" $PLIST
+
+# Print out file for reference
+cat $PLIST
+
+echo "Updated info.plist!"
